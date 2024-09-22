c19d8499c723457a51c9eb0cc27916382109f7cc
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-22 11:18:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn = document.getElemntbyId('incrementBtn');
+let counter = document.getElementId('counter');
+
+let count = 0;
+counter.innerHTML = count;
+btn.addEventListener('click',()=>{
+	alert(count);
+	count++;
+	counter.innerHTML = count;
+})
